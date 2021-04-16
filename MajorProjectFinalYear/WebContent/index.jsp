36c60e1d7e77d6695bb8c96cbc71c14e89dda177
samar <samar@DESKTOP-UARNON9>
2021-04-16 23:18:35 +0530

    question & answer update

--- MajorProjectFinalYear/WebContent/index.jsp
+++ MajorProjectFinalYear/WebContent/index.jsp
@@ -1,3 +1,6 @@
+<%@page import="dao.DAO"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
@@ -7,6 +10,41 @@
 <title>Insert title here</title>
 </head>
 <body>
-bsjfhgfhjfhwjfblsfbasndfhufbysf
+<%
+	String searchQues=request.getParameter("searchQues");
+	%>
+<form action="index.jsp">
+	<input type="text" name="searchQues" Placeholder="Search Questions"/>
+	<button type="submit">Search</button>
+</form>
+
+<%
+		
+		DAO dao=new DAO();
+		ArrayList<HashMap> questions=dao.getQuestion(searchQues);
+		try{
+		if(questions.size()!=0){
+		for(HashMap question:questions){
+	%>
+		qid: <%=question.get("quesId") %>,
+		<a href="ViewAnswer.jsp?quesId=<%=question.get("quesId") %>">question: <%=question.get("question") %> </a>,
+		studentId: <%=question.get("studentId") %>,
+		dateTime: <%=question.get("dateTime") %><br>
+		<hr>
+	<%
+		}
+		}else{
+			%>
+				<p>No data Found..</p>
+			<%
+		}
+		}catch(Exception e){
+			%>
+			<p>exception occured</p>
+			<%
+		}
+	%>
+
+
 </body>
 </html>
--- MajorProjectFinalYear/src/dao/DAO.java
+++ MajorProjectFinalYear/src/dao/DAO.java
@@ -27,4 +27,47 @@ public class DAO {
 			return null;
 		}
 	}
+	
+	public ArrayList<HashMap> getQuestion(String question) throws Exception {
+		PreparedStatement p = con.prepareStatement("select * from questions where question like ?");
+		p.setString(1, "%"+question+"%");
+		ResultSet rs = p.executeQuery();
+		ArrayList<HashMap> questions = new ArrayList();
+		while (rs.next()) {
+			HashMap ques = new HashMap();
+			ques.put("quesId", rs.getString("quesId"));
+			ques.put("question", rs.getString("question"));
+			ques.put("studentId", rs.getString("studentId"));
+			ques.put("dateTime", rs.getDate("dateTime"));
+			questions.add(ques);
+		}
+		return questions;
+	}
+	
+	public ArrayList<HashMap> getAnswer(int quesId) throws Exception {
+		PreparedStatement p = con.prepareStatement("select * from answers where qid= ?");
+		p.setInt(1, quesId);
+		ResultSet rs = p.executeQuery();
+		ArrayList<HashMap> answers = new ArrayList();
+		while (rs.next()) {
+			HashMap ans = new HashMap();
+			ans.put("qid", rs.getString("qid"));
+			ans.put("answers", rs.getString("answers"));
+			ans.put("studentId", rs.getString("studentId"));
+			ans.put("dateTime", rs.getDate("dateTime"));
+			answers.add(ans);
+		}
+		return answers;
+	}
+	
+	public String getQuestionById(int qid) throws Exception {
+		PreparedStatement p = con.prepareStatement("select * from questions where quesId=?");
+		p.setInt(1, qid);
+		ResultSet rs = p.executeQuery();
+		if (rs.next()) {
+			return rs.getString("question");
+		} else {
+			return null;
+		}
+	}
 }
